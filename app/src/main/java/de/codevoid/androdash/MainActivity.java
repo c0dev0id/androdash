f32f4f23b6f2f27e51d5a115619c29f5bb2057ad
c0dev0id <sh+git@codevoid.de>
2026-04-18 11:30:40 +0200

    Fix missing ViewParent import

--- app/src/main/java/de/codevoid/androdash/MainActivity.java
+++ app/src/main/java/de/codevoid/androdash/MainActivity.java
@@ -17,6 +17,7 @@ import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewParent;
 import android.view.ViewTreeObserver;
 import android.widget.FrameLayout;
 import android.widget.HorizontalScrollView;
